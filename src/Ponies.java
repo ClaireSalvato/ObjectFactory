09fcf58ce3c4252adbcd4e1df217f594a1f87fcd
ClaireSalvato <claire_salvato26@milton.edu>
2023-10-25 09:01:05 -0400

    Initial commit

---
+++ src/Ponies.java
@@ -0,0 +1,12 @@
+public class Ponies {
+    //true/false -> boolean
+    //numbers -> int, double
+    //words -> String
+
+    boolean isUnicorn = false;
+    boolean isPegasus = false;
+
+    double size = 20.56;
+    String name = "Thoroughbred";
+    String color = "Maroon";
+}
